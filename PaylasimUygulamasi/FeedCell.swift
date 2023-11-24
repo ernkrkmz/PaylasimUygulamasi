36a46ed87200056e883e050f95af637a725713a8
Ernkrkmz <eren_korkmaz_@hotmail.com>
2023-11-24 17:45:45 +0300

    firsth

---
+++ PaylasimUygulamasi/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  PaylasimUygulamasi
+//
+//  Created by Eren Korkmaz on 26.04.2023.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
